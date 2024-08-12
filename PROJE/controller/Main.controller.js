082a1cf91d2c0fd96431072a7b19af2cfe8fa0c3
Tuba Pesen <ptuba56@gmail.com>
2024-08-12 10:56:09 +0300

    add folder

---
+++ PROJE/controller/Main.controller.js
@@ -0,0 +1,37 @@
+
+
+sap.ui.define(
+	[
+	  "sap/ui/core/mvc/Controller",
+	  "sap/ui/model/json/JSONModel",
+	  "sap/m/MessageToast",
+	  "sap/m/MessageBox",
+	  "sap/ui/core/Fragment",
+	  "andonScreen/model/formatter",
+	  "common/transactionCaller"
+  
+	],
+	function (
+	  Controller,
+	  JSONModel,
+	  MessageToast,
+	  MessageBox,
+	  Fragment,
+	  formatter,
+	  TransactionCaller,
+  
+	) {
+	  "use strict";
+	  let tempData = [];
+	  let index = 0;
+	  return Controller.extend("andonScreen.controller.Main", {
+		onInit: function () {
+
+  
+		},
+		
+	  });
+	}
+  );
+
+  
